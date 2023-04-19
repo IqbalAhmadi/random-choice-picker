d3f442b3df1787bdb893623a457bad42d67addf8
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-04-18 22:09:51 -0700

    deleted the hardcoded html tags

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
   createTags(e.target.value)
+
+  // check to see if we hit enter
+  if (e.key === 'Enter') {
+    // clear the input area after 10ms
+    setTimeout(() => {
+      e.target.value = ''
+    }, 10)
+
+    randomSelect()
+  }
 })
 
 function createTags(input) {
@@ -23,4 +33,46 @@ function createTags(input) {
     tagEl.innerText = tag
     tagsEl.appendChild(tagEl)
   })
+}
+
+// create random select
+function randomSelect() {
+  const times = 30
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag()
+
+    if (randomTag !== undefined) {
+      highlightTag(randomTag)
+
+      setTimeout(() => {
+        unHighlightTag(randomTag)
+      }, 100)
+    }
+  }, 100)
+
+  setTimeout(() => {
+    clearInterval(interval)
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag()
+
+      highlightTag(randomTag)
+    }, 100)
+  }, times * 100)
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag')
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+
+// add highlight tag function
+function highlightTag(tag) {
+  tag.classList.add('highlight')
+}
+
+// remove highlight tag function
+function unHighlightTag(tag) {
+  tag.classList.remove('highlight')
 }
